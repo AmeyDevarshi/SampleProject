9a3160e3210dadfab64e5e9ef06faef63a3d7f8d
AmeyDevarshi <67369511+AmeyDevarshi@users.noreply.github.com>
2021-12-13 08:29:19 +0530

    Update reply.java

--- reply.java
+++ reply.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-        System.out.println("Hello, World!... This is a reply"); 
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey!!!!"); 
+    }
 }
